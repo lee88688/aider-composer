5c315fcd4c3b33012f4b8e497c9a9bda738ed9eb
lee <lee88688@163.com>
2025-02-08 19:16:09 +0800

    feat: make diff editor partial accept

--- src/diffView/diffEditor.ts
+++ src/diffView/diffEditor.ts
@@ -2,6 +2,30 @@ import { DiffViewManager } from './index';
 import * as vscode from 'vscode';
 import * as fsPromise from 'fs/promises';
 import path from 'path';
+import { diffLines } from 'diff';
+
+type RemovedChange = {
+  type: 'removed';
+  line: number;
+  count: number;
+  value: string;
+};
+
+type AddedChange = {
+  type: 'added';
+  line: number;
+  count: number;
+  value: string;
+};
+
+type Change =
+  | RemovedChange
+  | AddedChange
+  | {
+      type: 'modified';
+      removed: RemovedChange;
+      added: AddedChange;
+    };
 
 // add TextDocumentContentProvider class
 class DiffContentProvider implements vscode.TextDocumentContentProvider {
@@ -10,11 +34,24 @@ class DiffContentProvider implements vscode.TextDocumentContentProvider {
   }
 }
 
-export class DiffEditorViewManager extends DiffViewManager {
+export class DiffEditorViewManager
+  extends DiffViewManager
+  implements vscode.CodeLensProvider
+{
   static readonly DiffContentProviderId = 'aider-diff';
 
   // uri -> content
-  private fileChangeSet = new Map<string, string>();
+  private fileChangeMap = new Map<
+    string,
+    {
+      originalContent: string;
+      modifiedContent: string;
+      changes: Change[];
+    }
+  >();
+
+  private _onDidChangeCodeLenses = new vscode.EventEmitter<void>();
+  readonly onDidChangeCodeLenses = this._onDidChangeCodeLenses.event;
 
   constructor(
     private context: vscode.ExtensionContext,
@@ -31,6 +68,9 @@ export class DiffEditorViewManager extends DiffViewManager {
       );
 
     this.disposables.push(
+      this._onDidChangeCodeLenses,
+      vscode.languages.registerCodeLensProvider([{ scheme: 'file' }], this),
+
       providerRegistration,
       vscode.commands.registerCommand(
         'aider-composer.ConfirmModify',
@@ -47,7 +87,7 @@ export class DiffEditorViewManager extends DiffViewManager {
             outputChannel.error(`Error writing file: ${error}`);
           }
 
-          this.fileChangeSet.delete(uri.toString());
+          this.fileChangeMap.delete(uri.toString());
           this._onDidChange.fire({
             type: 'accept',
             path: fileUri.fsPath,
@@ -73,7 +113,7 @@ export class DiffEditorViewManager extends DiffViewManager {
           this.outputChannel.debug(
             `Diff document closed for: ${document.uri.path}`,
           );
-          this.fileChangeSet.delete(document.uri.toString());
+          this.fileChangeMap.delete(document.uri.toString());
           this._onDidChange.fire({
             type: 'reject',
             path: document.uri.fsPath,
@@ -81,6 +121,48 @@ export class DiffEditorViewManager extends DiffViewManager {
         }
       }),
     );
+  }
+
+  async provideCodeLenses(
+    document: vscode.TextDocument,
+  ): Promise<vscode.CodeLens[]> {
+    const fileUri = document.uri;
+    const fileChange = this.fileChangeMap.get(fileUri.toString());
+    if (!fileChange) {
+      return [];
+    }
+
+    console.log('codelens', document.uri);
+
+    const codeLenses: vscode.CodeLens[] = [];
+
+    for (let i = 0; i < fileChange.changes.length; i++) {
+      const change = fileChange.changes[i];
+
+      const line = change.type === 'modified' ? change.added.line : change.line;
+      const count =
+        change.type === 'modified' ? change.added.count : change.count;
+
+      const range = new vscode.Range(
+        new vscode.Position(line, 0),
+        new vscode.Position(line, 0),
+      );
+
+      codeLenses.push(
+        new vscode.CodeLens(range, {
+          title: 'Accept',
+          command: 'aider-composer.AcceptChange',
+          arguments: [document.uri.toString(), i],
+        }),
+        new vscode.CodeLens(range, {
+          title: 'Reject',
+          command: 'aider-composer.RejectChange',
+          arguments: [document.uri.toString(), i],
+        }),
+      );
+    }
+
+    return codeLenses;
   }
 
   async openDiffView(data: { path: string; content: string }): Promise<void> {
@@ -93,6 +175,69 @@ export class DiffEditorViewManager extends DiffViewManager {
       isNewFile = true;
     }
 
+    const uri = vscode.Uri.file(data.path);
+    let originalContent = '';
+    if (this.fileChangeMap.has(uri.toString())) {
+      const fileChange = this.fileChangeMap.get(uri.toString())!;
+      originalContent = fileChange.originalContent;
+    } else {
+      originalContent = await vscode.workspace.fs
+        .readFile(vscode.Uri.file(data.path))
+        .then((buffer) => Buffer.from(buffer).toString('utf-8'));
+    }
+
+    const modifiedContent = data.content;
+    const differences = diffLines(originalContent, modifiedContent);
+
+    let lineNumber = 0;
+    const changes: Change[] = [];
+    let lastRemoved: RemovedChange | undefined;
+
+    for (const part of differences) {
+      let currentChange: Change | undefined;
+
+      if (part.removed) {
+        lastRemoved = {
+          type: 'removed',
+          line: lineNumber,
+          count: part.count!,
+          value: part.value,
+        };
+        // the last removed part should not wait for the next added part
+        if (part === differences[differences.length - 1]) {
+          currentChange = lastRemoved;
+        }
+      } else if (part.added) {
+        lineNumber += part.count!;
+        const added: AddedChange = {
+          type: 'added',
+          line: lineNumber,
+          count: part.count!,
+          value: part.value,
+        };
+        if (lastRemoved) {
+          currentChange = {
+            type: 'modified',
+            removed: lastRemoved,
+            added,
+          };
+          lastRemoved = undefined;
+        } else {
+          currentChange = added;
+        }
+      } else {
+        lineNumber += part.count!;
+        if (lastRemoved) {
+          currentChange = lastRemoved;
+          lastRemoved = undefined;
+        }
+      }
+
+      if (currentChange) {
+        changes.push(currentChange);
+      }
+    }
+
     try {
       const originalUri = isNewFile
         ? vscode.Uri.parse(
@@ -100,12 +245,18 @@ export class DiffEditorViewManager extends DiffViewManager {
           ).with({
             query: Buffer.from('').toString('base64'),
           })
-        : vscode.Uri.file(data.path);
-      const modifiedUri = vscode.Uri.parse(
-        `${DiffEditorViewManager.DiffContentProviderId}:${data.path}`,
-      ).with({
-        query: Buffer.from(data.content).toString('base64'),
-      });
+        : vscode.Uri.parse(
+            `${DiffEditorViewManager.DiffContentProviderId}:${data.path}`,
+          ).with({
+            query: Buffer.from(originalContent).toString('base64'),
+          });
+
+      const modifiedUri = vscode.Uri.file(data.path); // .with({ query: 'isDiff' });
+
+      await vscode.workspace.fs.writeFile(
+        modifiedUri,
+        Buffer.from(modifiedContent),
+      );
 
       const name = path.basename(data.path);
 
@@ -118,13 +269,19 @@ export class DiffEditorViewManager extends DiffViewManager {
         {
           viewColumn: vscode.ViewColumn.Two,
           preview: false,
+          renderSideBySide: true,
         },
       );
     } catch (error) {
       this.outputChannel.error(`Error opening diff: ${error}`);
     }
 
-    this.fileChangeSet.set(vscode.Uri.file(data.path).toString(), data.content);
+    this.fileChangeMap.set(uri.toString(), {
+      originalContent: originalContent,
+      modifiedContent: modifiedContent,
+      changes: changes,
+    });
+
     this._onDidChange.fire({
       type: 'add',
       path: data.path,
@@ -152,14 +309,14 @@ export class DiffEditorViewManager extends DiffViewManager {
     }
 
     // Clear the file change set after closing all editors
-    this.fileChangeSet.clear();
+    this.fileChangeMap.clear();
   }
 
   async acceptAllFile(): Promise<void> {
-    for (const [uri, content] of this.fileChangeSet.entries()) {
+    for (const [uri, content] of this.fileChangeMap.entries()) {
       await vscode.workspace.fs.writeFile(
         vscode.Uri.parse(uri),
-        Buffer.from(content),
+        Buffer.from(''),
       );
     }
     await this.closeAllDiffEditor();
@@ -191,15 +348,15 @@ export class DiffEditorViewManager extends DiffViewManager {
     }
 
     // Remove the file from the change set
-    this.fileChangeSet.delete(targetUri);
+    this.fileChangeMap.delete(targetUri);
   }
 
   async acceptFile(path: string): Promise<void> {
     const uri = vscode.Uri.file(path);
-    const content = this.fileChangeSet.get(uri.toString());
+    const content = this.fileChangeMap.get(uri.toString());
 
     if (content) {
-      await vscode.workspace.fs.writeFile(uri, Buffer.from(content));
+      await vscode.workspace.fs.writeFile(uri, Buffer.from(''));
       await this.closeDiffEditor(path);
     }
   }
