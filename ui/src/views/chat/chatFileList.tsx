f5fce6deb290b10f7a09c5a2569822d5db3dee9a
gkzhb <gkzhb98@gmail.com>
2025-03-16 00:18:06 +0800

    chore: add parentheses

--- ui/src/views/chat/chatFileList.tsx
+++ ui/src/views/chat/chatFileList.tsx
@@ -212,7 +212,7 @@ function FileSearchList() {
                       fontSize: 12,
                     }}
                   >
-                    current file
+                    (current file)
                   </span>
                 )}
               </ListItem>
